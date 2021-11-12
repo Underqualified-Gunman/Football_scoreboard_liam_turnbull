68386233e1df714ecd1f14b6b767368b5f5d99c0
LiamTurnbull <ghostboy21134@gmail.com>
2021-11-12 00:58:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "scoreboard.h"
+
+
+int main() 
+{
+
+
+
+
+
+
+
+
+} 
---
+++ scoreboard.h
@@ -0,0 +1,26 @@
+class team
+{
+  
+
+
+
+
+
+
+
+
+
+};
+
+class scoreboard
+{
+
+
+
+
+
+
+
+};
+
+
